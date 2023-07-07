69c847755048153a5dfa61110046c6728dc3e9f6
Hassan arab <arabh60@gmail.com>
2023-07-07 18:40:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<?php
+include 'templates/header.php';
+?>
+
+<div id="carouselExampleSlidesOnly" class="carousel slide p-2" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/imgs/image1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/imgs/image2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/imgs/image3.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+</div>
+
+<!-- section  -->
+<div class="section mb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 wow slideInLeft">
+                <div class="first p-5 ">
+                    <h2>GREEN LIFE PRODUCTS ARE ALWAYS AVAILABLE WITH HIGH QUALITY AND COMPETITIVE PRICES TO SUPPORT MERCHANTS AND MEET MARKET NEEDS AT ANY TIME
+                    </h2>
+                    <div class="horizontal-line"></div>
+                    <p class="text-1">Green Life works around the clock to provide all the market needs of its products, which are characterized by their high quality and competitive prices, through a professional team of customer service working to receive your orders and deliver them to you as soon as possible.
+
+                    </p>
+                    <button class="btn btn-success rounded-pill ">call now <i class="fa-solid fa-headset"></i></button>
+                </div>
+            </div>
+            <div class="col-md-6 wow slideInRight">
+                <div class="second p-5     ">
+                    <img src="assets/imgs/image4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- about  -->
+<div class="about mt-5 green">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 wow slideInLeft">
+                <img src="assets/imgs/image5.png" class="w-75 ms-4" alt="">
+            </div>
+            <div class="col-md-6  wow zoomIn">
+                <h1>ABOUT COMPANY</h1>
+                <h3>Green Life for water treatment and purfication</h3>
+                <p class="p-2"> household drinking water filters, seeing that working in a field that improves human health and saves it from countless harms adds real value and a positive contribution to the benefit of society.
+                    After several successes, Green Life is now one of the largest suppliers of water filters, drinking stations and spare parts in Egypt, Africa and the Middle Eas</p>
+                <button class="btn btn-success">read more <i class="fa-brands fa-readme"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- our product  -->
+<div class="our-product">
+    <p>our<br> products</p>
+    <p>Green Life products are numerous in the field of water purification and treatment to include domestic and industrial use and water desalination plants<br>
+
+        <button class="btn btn-primary mt-4 rounded-pill p-3">view all</button>
+    </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="assets/imgs/product1.png" alt="">
+                <a href="#">RO TANK</a><br>
+                <button class="btn btn-primary mt-1 p-2">order now <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/imgs/product2.png" alt="">
+                <a href="#">BOLLARDS</a><br>
+                <button class="btn btn-primary mt-1 p-2">order now <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+            </div>
+            <div class="col-md-3  ">
+                <img src="assets/imgs/product3.png" alt="">
+                <a href="#">STAGE HOUSEHOLD FILTER 7</a><br>
+                <button class="btn btn-primary mt-1   p-2">order now <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+            </div>
+            <div class="col-md-3">
+                <img src="assets/imgs/product4.png" alt="">
+                <a href="#">INDUSTRIAL FILTER</a><br>
+                <button class="btn btn-primary mt-1 p-2">order now <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 wow fadeInLeft">
+                <div class="seconds ">
+                    <h2>OUR LATEST PRODUCT
+                    </h2>
+                    <div class="horizontal-line"></div>
+                    <p class="text-2">
+                        Green Life always strives to develop and provide everything new to meet the needs of local and international markets and maintain the health of the consumer, who always strives to obtain high-quality products at an affordable price.
+                    </p>
+                    <button class="btn btn-success rounded-pill">view products <i class="fa-regular fa-eye"></i></button>
+                </div>
+            </div>
+            <div class="col-md-6 wow fadeOutUp">
+                <div class=" p-5 image-second">
+                    <img src="assets/imgs/image8.png" alt="">
+                </div>
+            </div>
+        </div>
+        <h2 class="text-center green">GREEN LIFE PRODUCTS</h2>
+        <div class="green-product">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="assets/imgs/image11.png" alt=""><br>
+                    <i class="fa-solid fa-circle-right"></i>
+                </div>
+                <div class="col-md-4">
+                    <img src="assets/imgs/image9.png" alt=""><br>
+                    <i class="fa-solid fa-circle-right"></i>
+                </div>
+                <div class="col-md-4">
+                    <img src="assets/imgs/image10.png" alt=""><br>
+                    <i class="fa-solid fa-circle-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php
+include 'templates/footer.php';
---
+++ templates/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/all.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/animate.css">
+
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light  ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="../assets/imgs/green-life.png" style="width:100px" class="p-2 ms-4"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse pe-3" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">call us <i class="fa-solid fa-phone blue"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products <i class="fa-solid fa-glass-water blue"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">our services <i class="fa-brands fa-aws blue"></i></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            about company
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item  " href="#">company history <i class="fa-solid fa-clock-rotate-left blue"></i></a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
